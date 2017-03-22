125b2107160be7f7a1d6e4c576328a575ad5df37
0mpurdy <0mpurdy@gmail.com>
2017-03-22 22:56:41 +0000

    Fix node server function order

--- server.js
+++ server.js
@@ -24,12 +24,16 @@ const path = require('path');
 // // middleware
 // app.use(forceSSL());
 
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.use(express.static(__dirname + '/dist'));
+console.log('Started');
 
-app.listen(process.env.PORT || 8080);
