45f255ef30bb9df24970207319b9d98b7497f0c3
0mpurdy <0mpurdy@gmail.com>
2017-03-22 22:44:11 +0000

    Add express for Heroku

---
+++ server.js
@@ -0,0 +1,35 @@
+// https://medium.com/@ryanchenkie_40935/angular-cli-deployment-host-your-angular-2-app-on-heroku-3f266f13f352#.gkem42f6h
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// https://devcenter.heroku.com/articles/ssl
+// // If an incoming request uses
+// // a protocol other than HTTPS,
+// // redirect that request to the
+// // same url but with HTTPS
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
